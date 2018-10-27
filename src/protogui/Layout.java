4a49781bb4076d1872af9bc278c77d3fed4424b4
Calvin Roberts <r.calvin22@yahoo.com>
2018-10-26 20:58:19 -0400

    [added buttonsd]

--- src/protogui/Layout.java
+++ src/protogui/Layout.java
@@ -5,8 +5,12 @@
  */
 package protogui;
 
+import java.awt.*;
+import java.awt.event.*;
 import java.awt.FlowLayout;
+import javax.swing.*;
 import javax.swing.JFrame;
+import javax.swing.JScrollPane;
 import javax.swing.JLabel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -24,8 +28,13 @@ public class Layout extends JFrame implements ActionListener {
     private JLabel addLabel, genLabel, subLabel, multLabel, divLabel;
     JTextField addinput, output, subInput, multInput, divInput;
     JButton print;
+    //JFrame frame2;
+    JPanel panel, panel2, panel3, panel4, panel5;
+    JScrollPane scroll;
     MathFunctions mathFunctions = new MathFunctions();
     WriteFile writeFile = new WriteFile();
+   
+    
 
     public Layout() {
         super("MathGenerator");
@@ -75,7 +84,73 @@ public class Layout extends JFrame implements ActionListener {
         print.setBounds(20, 200, 170, 20);
         print.addActionListener(this);
         add(print);
+        
+        JRadioButton y,n,c;
+        panel = new JPanel();
+        ButtonGroup buttonGroup = new ButtonGroup();
+        y = new JRadioButton("easy");
+        buttonGroup.add(y);
+        panel.add(y);
+        n = new JRadioButton("normal");
+        buttonGroup.add(n);
+        panel.add(n);
+        c = new JRadioButton("hard");
+        buttonGroup.add(c);
+        panel.add(c);
+        panel.setBounds(190, 60, 210, 35);
+        add(panel);
+        
+        panel2 = new JPanel();
+        y = new JRadioButton("easy");
+        buttonGroup.add(y);
+        panel2.add(y);
+        n = new JRadioButton("normal");
+        buttonGroup.add(n);
+        panel2.add(n);
+        c = new JRadioButton("hard");
+        buttonGroup.add(c);
+        panel2.add(c);
+        panel2.setBounds(190, 95, 210, 35);
+        add(panel2);
+        
+        panel3 = new JPanel();
+        y = new JRadioButton("easy");
+        buttonGroup.add(y);
+        panel3.add(y);
+        n = new JRadioButton("normal");
+        buttonGroup.add(n);
+        panel3.add(n);
+        c = new JRadioButton("hard");
+        buttonGroup.add(c);
+        panel3.add(c);
+        panel3.setBounds(190, 125, 210, 35);
+        add(panel3);
+       
+        panel4 = new JPanel();
+        y = new JRadioButton("easy");
+        buttonGroup.add(y);
+        panel4.add(y);
+        n = new JRadioButton("normal");
+        buttonGroup.add(n);
+        panel4.add(n);
+        c = new JRadioButton("hard");
+        buttonGroup.add(c);
+        panel4.add(c);
+        panel4.setBounds(190, 155, 210, 35);
+        add(panel4);
+        
+       panel5 = new JPanel();
+       scroll = new JScrollPane();
+       JScrollPane scrollBar=new JScrollPane(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+       panel5.add(scrollBar);
+       panel5.setSize(400,400); 
+       panel5.setVisible(true);
+       panel5.setBounds(500, 800, 50, 70);
+       add(panel5);
+       //add(scroll);
+      
     }
+    
 
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == print) {
